c0cf01d568e429d95009c5fefcf5af0be667e365
Unknown <admin@woodleader.org>
2016-11-14 11:03:44 +0100

    Readability

--- Arbeitsblatt 3/Aufgabe 5/Range.java
+++ Arbeitsblatt 3/Aufgabe 5/Range.java
@@ -4,9 +4,9 @@ public class Range {
 		int end = Integer.parseInt(args [1]);
 		int step = Integer.parseInt (args [2]);
 		int i = start;
-		while (i != end && i < (end+1)) {
+		while (i != end && i < end) {
 			System.out.println(i);
-			i = i + step;
+			i = i + step; 
 		}
 	}
 }
