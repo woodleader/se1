5fd5398cadf609f6f3371727234a29e6d09205de
Unknown <admin@woodleader.org>
2016-11-15 15:54:55 +0100

    Changed Range.Java
    Overflow with step <= 0

--- Arbeitsblatt 4/Aufgabe 2/c/Range.java
+++ Arbeitsblatt 4/Aufgabe 2/c/Range.java
@@ -4,9 +4,14 @@ public class Range {
 		int end = Integer.parseInt(args [1]);
 		int step = Integer.parseInt (args [2]);
 		int i = start;
-		while (i != end && i < end) {
+		if (step <= 0) {
+			System.out.println("Step must be > 0");
+		}
+		else {
+			while (i != end && i < end) {
 			System.out.println(i);
 			i = i + step; 
+			}	
 		}
 	}
 }
